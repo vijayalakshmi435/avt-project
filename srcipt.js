fbb431da4fa632ea5149360a8793d3e84d9f3b37
vijayalakshmi <mamillavijayalakshmi789@gmail.com>
2022-12-09 10:43:53 +0530

    ok

---
+++ srcipt.js
@@ -0,0 +1,30 @@
+const sub=()=>{
+    let a= document.getElementById('in1').value;
+    let b =document.getElementById('in2').value;
+    let c =document.getElementById('in3').value;
+    let d =document.getElementById('in4').value;
+    let e =document.getElementById('in5').value;
+    let f =document.getElementById('in6').value;
+    
+
+
+
+    console.log(a,b,c,d,e,f)
+    if(a=="" && b=="" &&c==""&&d==""&&e=="" &&f==""){
+        alert('fill the form')
+    }
+
+    else{
+        alert('submit done')
+    }
+
+
+    
+}
+
+
+
+
+const card=()=>{
+    document.getElementById('pop').value;
+}
